451a0bbd5a4bd5a61167db40201ebffca2d79f62
Сергей Б <human710@gmail.com>
2022-07-15 21:54:22 +0300

    Готово

---
+++ HW_9_TelegramBot/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace HW_9_TelegramBot
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //new TelegrammClass();
+            try
+            {
+                TelegramBotClass bot = new TelegramBotClass(token: "5292001260:AAECjsY3A-tKHtedpt76BlR68uprUnicDr8");
+                bot.GetUpdates();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                
+            }
+        }
+    }
+}
